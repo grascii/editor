44b6303bbe2345bb7c07b1ed4368557b5cd97456
Wyess <diyhacker@gmail.com>
2020-10-06 21:37:44 +0900

    Saving/Loading feature is much more stable now.

--- shorthand_editor.js
+++ shorthand_editor.js
@@ -11,10 +11,6 @@ svg.setAttribute("data-text", "");
 svg.setAttribute("transform", "scale(1, 1) translate(0)");
 svg.setAttribute("style", "fill:none;stroke:#000000;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-opacity:1");
 const g = document.createElementNS("http://www.w3.org/2000/svg", "g");
-//g.setAttribute("id", "page");
-//g.setAttribute("transform", "scale(1, 1) translate(0)");
-//g.setAttribute("style", "fill:none;stroke:#000000;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-opacity:1");
-//svg.appendChild(g);
 viewer.appendChild(svg);
 const styleAnimation = document.createElement("style");
 styleAnimation.setAttribute("id", "style_animation");
@@ -32,22 +28,69 @@ const downloadButtonPNG = document.getElementById("download_button_png");
 downloadButtonPNG.addEventListener("click", downloadPNG);
 
 const save_button = document.getElementById("save_button");
-save_button.addEventListener("click", savePage);
+save_button.addEventListener("click", savePages);
 
-const pageList = document.getElementById("savedPageList");
+
 const storage = localStorage;
-console.log(storage.getItem("new_page"));
-input.value = storage.getItem(storage.key(0));
-//input.value = storage.getItem(storage.getItem("new_page")); //|| storage.getItem(storage.key(0));
-for (var i = 0; i < storage.length; i++) {
-  const opt = document.createElement("option");
-  opt.text = storage.key(i);
-  opt.value = storage.key(i);
-  pageList.add(opt);
-}
-pageList.size = storage.length;
+const savedPages = storage.getItem("pages");
+const pages = savedPages ? JSON.parse(savedPages) : {};
+const pageList = document.getElementById("saved_page_list");
+const pageNameInput = document.getElementById("page_name");
+const inputTextArea = document.getElementById("input");
 
-fileNameInput.value = storage.key(0); 
+loadPageList();
+resume();
+openPage();
+
+function loadPageList() {
+  const titles = Object.keys(pages);
+  titles.sort();
+
+  pageList.innerText = "";
+  titles.forEach(function(title) {
+    const opt = document.createElement("option");
+    opt.text = title;
+    opt.value = title;
+    pageList.add(opt);
+  });
+}
+
+function savePages() {
+  const text = inputTextArea.value;
+  const title = pageNameInput.value;
+
+  if (text == "") {
+    delete pages[title];
+    pageNameInput.value = "";
+  } else if (title != "") {
+    pages[title] = text;
+  }
+
+  storage.setItem("pages", JSON.stringify(pages));
+  loadPageList();
+  openPage();
+}
+
+function openPage() {
+  const title = pageList.value;
+  if (title) {
+    pageNameInput.value = title;
+    inputTextArea.value = pages[title];
+    storage.setItem("lastPage", pageList.value);
+    updateSVG();
+  }
+}
+
+function resume() {
+  const lastPage = storage.getItem("lastPage");
+
+  if (lastPage && pages[lastPage]) {
+    pageList.value = lastPage;
+    pageNameInput.value = lastPage;
+    inputTextArea.value = pages[lastPage];
+  }
+}
+
 
 const event = new Event("input", {
   bubbles: true,
@@ -56,16 +99,7 @@ const event = new Event("input", {
 input.dispatchEvent(event);
 
 
- 
-
-const divObjects = document.getElementById("objects");
-//divObjects.innerHTML = Object.keys(window).toString().split(",").filter(function(el) {
-//  return el.search(/^(?:Waseda|Nakane|Svsd|Shugiin|Char)/) !== -1;
-//}).join("<br>");
-
 function setMirrorMode(mirrored) {
-  //const g = document.getElementById("page");
-  //g.setAttribute("transform", "scale(" + (mirrored ? -1 : 1) + ", 1) translate(" + (mirrored ? -svg.viewBox.baseVal.width : 0) + ")");
   svg.setAttribute("transform", "scale(" + (mirrored ? -1 : 1) + ", 1) translate(" + (mirrored ? -svg.viewBox.baseVal.width : 0) + ")");
 }
 
@@ -104,42 +138,6 @@ function downloadPNG() {
     }, 10);
   };
   img.src = "data:image/svg+xml," + encodeURIComponent(svgStr);
-}
-
-function savePage() {
-  const storage = localStorage;
-  const name = fileNameInput.value || "new_page";
-  const text = document.getElementById("input").value;
-  if (text == "") {
-    for (var i = storage.length - 1; i >= 0; i--) {
-      pageList.remove(i);
-    }
-    storage.removeItem(name);
-    for (var i = 0; i < storage.length; i++) {
-      const opt = document.createElement("option");
-      opt.text = storage.key(i);
-      opt.value = storage.key(i);
-      pageList.add(opt);
-    }
-  } else {
-    if (storage.getItem(name) === null) {
-      const opt = document.createElement("option");
-      opt.text = name;
-      opt.value = name;
-      pageList.add(opt);
-    }
-    storage.setItem(name, text);
-  }
-  pageList.size = storage.length;
-}
-
-function openSavedPage(e) {
-  const storage = localStorage;
-  const idx = pageList.selectedIndex;
-  fileNameInput.value = storage.key(idx); 
-  storage.setItem("new_page", storage.key(idx));
-  document.getElementById("input").value = storage.getItem(pageList.options[idx].value);
-  input.dispatchEvent(event);
 }
 
 function parseInputText(text) {
@@ -229,80 +227,8 @@ function parseInputText(text) {
   return chars;
 }
 
-function _parseInputText(text) {
-  const tokens = text.match(/[↑↓←→][\d０-９]+(?:[.．][\d０-９]+)?|\\[a-z]+[ ]*\n?|（[^）]*）|\([^)]*\)|.[ゃゅょぁぃぅぇぉ]?ー?ん?/gsu) || [];
-  console.log(tokens);
-
-  var shorthand = "none";
-
-  const chars = [];
-  for (var i = 0; i < tokens.length; i++) {
-    var tok = tokens[i].replace(/^[(（]|[）)]$/g, ""); 
-    if (tok.charAt(0) === "\\") {
-      shorthand = tok.replace(/[\s\\]/g, "") || "none";
-   // }
-   // else if (shorthand === "none") {
-   //   var text = "";
-   //   while ((i < tokens.length) && (tokens[i].charAt(0) !== "\\") && (tokens[i].charAt(0) !== "\n")) {
-   //     text += tokens[i];
-   //     i++;
-   //   }
-   //   chars.push(new CharText(text));
-    } else if (/^[↑↓←→]-?[\d０-９]+/.test(tok)) {
-      var f = tok.match(/^[↑↓←→]|[-ー]?[\d０-９]+(?:\.[\d０-９]+)?/gu);
-      f[1] = f[1].replace(/０/g, "0")
-                 .replace(/１/g, "1")
-                 .replace(/２/g, "2")
-                 .replace(/３/g, "3")
-                 .replace(/４/g, "4")
-                 .replace(/５/g, "5")
-                 .replace(/６/g, "6")
-                 .replace(/７/g, "7")
-                 .replace(/８/g, "8")
-                 .replace(/９/g, "9")
-                 .replace(/ー/g, "-")
-                 .replace(/．/g, ".");
-      f[1] = parseInt(f[1], 10);
-      switch (f[0]) {
-        case "↑":
-          chars.push(new CharUp(f[1]));
-          break;
-        case "↓":
-          chars.push(new CharDown(f[1]));
-          break;
-        case "←":
-          chars.push(new CharLeft(f[1]));
-          break;
-        case "→":
-          chars.push(new CharRight(f[1]));
-          break;
-        default:
-          break;
-      }
-    } else if (Char.dict[tok]) {
-      chars.push(new Char.dict[tok]());
-    } else if (shorthand === "none") {
-      chars.push(new CharText(tok));
-    } else if (shorthand === "waseda" && WasedaChar.dict[tok]) {
-      chars.push(new WasedaChar.dict[tok]());
-    } else if (shorthand === "nakane" && NakaneChar.dict[tok]) {
-      chars.push(new NakaneChar.dict[tok]());
-    } else if (shorthand === "svsd" && SvsdChar.dict[tok]) {
-      chars.push(new SvsdChar.dict[tok]());
-    } else if (shorthand === "shugiin" && ShugiinChar.dict[tok]) {
-      chars.push(new ShugiinChar.dict[tok]());
-    } else if (shorthand === "takusari" && TakusariChar.dict[tok]) {
-      chars.push(new TakusariChar.dict[tok]());
-    } else {
-      chars.push(new CharText("\u25A1"));
-    }
-  };
-
-  return chars;
-}
 
 function setAnimation(speed) {
-  //const paths = page.querySelectorAll("path");
   const svg = document.getElementById("svg_root");
   const paths = svg.querySelectorAll("path");
 
@@ -314,9 +240,9 @@ function setAnimation(speed) {
   var style = "@keyframes shorthand_draw{100%{stroke-dashoffset:0;}}";
 
   paths.forEach(function(path, i) {
-    const margin = 1;
-    const dash = Math.round(path.getTotalLength());
-    const duration_ms = Math.round(dash / speed);
+    const margin = 0.5;
+    const dash = (path.getTotalLength());
+    const duration_ms = (dash / speed);
     path.setAttribute("class", "shorthand_" + i);
     style += ".shorthand_" + i + "{" +
              "animation:shorthand_draw " + duration_ms + "ms linear " + start_ms + "ms forwards;" +
@@ -326,12 +252,10 @@ function setAnimation(speed) {
   });
 
   const style_new = document.createElementNS("http://www.w3.org/2000/svg", "style");
-  //const style_old = document.getElementById("style_animation");
   style_new.setAttribute("id", "style_animation");
   style_new.textContent = style;
 
   svg.appendChild(style_new);
-  //svg.replaceChild(style_new, style_old);
 }
 
 function updateSVG(toAnimate) {
@@ -341,7 +265,6 @@ function updateSVG(toAnimate) {
   svg.setAttribute("data-text", encodeURIComponent(text));
 
   svg.textContent = "";
-  //svg.appendChild(styleAnimation);
   svg.appendChild(Char.createElements(chars, {x: 5, y: 10, left: 5, right: 5, bottom: 10, row: 10}));
   if (toAnimate) {
     setAnimation();
